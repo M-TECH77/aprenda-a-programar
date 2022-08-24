8b4ccfd05a26306d9b1c08fa6eb05e423ab749ab
Marcelino <marcelinocost69@gmail.com>
2022-08-24 11:05:33 -0300

    Atualização em pratica-1.js

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -4,10 +4,23 @@ let profissao
 let idade
 let lugarParaConhecer
 
-nome = "Marcelino"
-email = "marcelinocost69@gmail.com"
-profissao = "Dev Estudante"
+nome = " Marcelino\n"
+email = "marcelinocost69@gmail.com\n"
+profissao = "Dev Estudante\n"
 idade = 54
-lugarParaConhecer = "Portugal"
+paisParaConhecer = "Portugal"
+
+console.log(
+  " Nome:",
+  nome,
+  "E-mail:",
+  email,
+  "Profissão:",
+  profissao,
+  "Idade:",
+  idade,
+  "\n País:",
+  paisParaConhecer
+)
 
 // Execute: (git status -> git add * -> git commit -m 'mensagem de commit' -> git log -> git push) para subir a aplicação
