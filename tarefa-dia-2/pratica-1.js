cd975b952fa38a49aeb2a1220aa81a908a2088f7
Marcelino <marcelinocost69@gmail.com>
2022-08-24 11:32:20 -0300

    Desafio Opcional pratica-1.js

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -13,7 +13,17 @@ paisParaConhecer = "\n Portugal"
 function ituring() {
   console.log(nome, email, profissao, idade, paisParaConhecer)
 }
-
 ituring()
+
+console.log(`Olá, ${nome}! 
+Somos aqui da agência de turismo Viagem dos Sonhos. 
+Estamos te escrevendo este email, pois acabamos de 
+confirmar as compras das passagens aéreas para 
+${paisParaConhecer}. 
+Confirma pra gente alguns dados? A sua idade é 
+${idade}, você é 
+${profissao} e o seu email é 
+${email}? 
+Ficamos no aguardo. Muito obrigado e boa viagem!`)
 
 // Execute: (git status -> git add * -> git commit -m 'mensagem de commit' -> git log -> git push) para subir a aplicação
