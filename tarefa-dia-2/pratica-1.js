52d7ddd3ef660b617d7d914c3f0c6129887203aa
Marcelino <marcelinocost69@gmail.com>
2022-08-24 10:33:02 -0300

    commit tarefa-dia-2/pratica-1.js

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,7 @@
+let nome = "Marcelino"
+let email = "marcelinocost69@gmail.com"
+let profissao = "Dev Estudante"
+let idade = 54
+let lugarParaConhecer = "Portugal"
+
+// Execute: (git status -> git add * -> git commit -m 'mensagem de commit' -> git log -> git push) para subir a aplicação
