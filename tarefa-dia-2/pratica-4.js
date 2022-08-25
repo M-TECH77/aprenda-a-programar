3e71aa662a85d9cb8c754008a2f49e78fa81cc16
Marcelino <marcelinocost69@gmail.com>
2022-08-25 18:03:50 -0300

    Commit em tarefa-dia-2/pratica-4.js

---
+++ tarefa-dia-2/pratica-4.js
@@ -0,0 +1,64 @@
+let produtoA = {
+  nomeProduto: "Camiseta:\n",
+  valorProduto: 100.0,
+  produtoInternacional: true,
+  impostoSobreProd: 1.2,
+}
+
+function produtoITuringCamiseta() {
+  if (produtoA.produtoInternacional === true) {
+    console.log(`${produtoA.nomeProduto} R$ 100 + imposto de 20%`)
+    console.log(
+      " Total: R$",
+      produtoA.valorProduto * produtoA.impostoSobreProd
+    ) / 100
+  } else {
+    console.log("Desculpe! Produto não disponível no momento!")
+  }
+}
+produtoITuringCamiseta()
+
+console.log()
+let produtoB = {
+  nomeProduto: "Perfume:\n",
+  valorProduto: 200.0,
+  produtoInternacional: true,
+  impostoSobreProd: 1.2,
+}
+
+function produtoITuringPerfume() {
+  if (produtoB.produtoInternacional === true) {
+    console.log(`${produtoB.nomeProduto} R$ 200 + imposto de 20%`)
+    console.log(
+      " Total: R$",
+      produtoB.valorProduto * produtoB.impostoSobreProd
+    ) / 100
+  } else {
+    console.log("Desculpe! Produto não disponível no momento!")
+  }
+}
+produtoITuringPerfume()
+
+console.log()
+let produtoC = {
+  nomeProduto: "Sandália:\n",
+  valorProduto: 120.0,
+  produtoInternacional: true,
+  impostoSobreProd: 1.12,
+}
+
+function produtoITuringSandalia() {
+  if (produtoC.produtoInternacional === true) {
+    console.log(`${produtoC.nomeProduto} R$ 120 + imposto de 12%`)
+    console.log(
+      " Total: R$",
+      produtoC.valorProduto * produtoC.impostoSobreProd
+    ) / 100
+  } else {
+    console.log("Desculpe! Produto não disponível no momento!")
+  }
+}
+produtoITuringSandalia()
+
+console.log()
+console.log("Obrigado pela preferência e volte sempre!")
